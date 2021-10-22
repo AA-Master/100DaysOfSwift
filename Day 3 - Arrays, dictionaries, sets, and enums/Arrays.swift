e4da018257d58c70c8a77baedd61de0531c86f8b
Anwar Abdeljaber <35644359+AA-Master@users.noreply.github.com>
2021-10-21 22:57:35 -0500

    Create Arrays.swift

---
+++ Day 3 - Arrays, dictionaries, sets, and enums/Arrays.swift
@@ -0,0 +1,25 @@
+//How to initialize an array
+var numbers = [6, 2, 4]
+
+//How to add elements to an array
+numbers.append(5)
+
+//Access an array by providing the element number e.g (0-2)
+print(numbers[3])
+
+//Count the amount of elements an array
+print(numbers.count)
+
+//Removes an element at the 1 index
+print(numbers.remove(at: 1))
+
+//Displays the updated array
+print(numbers)
+
+let num2 = numbers.sorted()
+print(num2)
+
+/*
+-Learned how to add, remove, reverse, and access arrays
+-Strings and Int types can't be in the same array
+*/
