59676754c37ab59ad97e7953069deb2f0c641622
Anwar Abdeljaber <35644359+AA-Master@users.noreply.github.com>
2021-10-23 23:51:50 -0500

    Create Conditions.swift
    Today I learned about:
    
    - If, switch, and ternary operators

---
+++ Day 5 - if, switch, and the ternary operator/Conditions.swift
@@ -0,0 +1,31 @@
+//Day 5
+//if, switch, and the ternary operator
+
+let score = 60
+
+if score > 80 {
+    print("great job")
+}
+
+else {
+    print("yuck")
+}
+
+let anwar = [2]
+
+if anwar == [2]{
+    
+    print("yes, this works!")
+}
+
+else {
+    
+    print("It doesn't work")
+}
+
+var username = ""
+
+if username.isEmpty == true {
+    username = "Anonymous"
+    print(username)
+}
